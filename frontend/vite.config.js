f88d0f0a04903684927f34ea60228f6eb9e5b58d
NikitaPoraiyar <poraiyarnikita@gmail.com>
2025-05-02 15:46:50 +0530

    Initial commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Add an alias if needed, but generally, you shouldn't need it for recharts
+      'recharts': 'recharts/dist/umd/Recharts.min.js',
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['recharts'], // If you want to externalize recharts
+    },
+  },
 })
